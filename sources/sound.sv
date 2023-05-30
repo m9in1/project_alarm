8500435503f63bbf9831f4037fdff8bcccdc1c57
m9in1 <kirill000ss56@gmail.com>
2023-05-30 22:33:42 +0300

    Honor init

---
+++ sources/sound.sv
@@ -0,0 +1,13 @@
+`define APB_WIDTH 32
+`define DIS_FREQ 22000
+
+
+module sound(
+	input clk,
+	input [`APB_WIDTH-1:0] 	datadig_i,
+	output [`APB_WIDTH-1:0]	dataan_o,
+	output data_ready
+	);
+
+	
+endmodule;
